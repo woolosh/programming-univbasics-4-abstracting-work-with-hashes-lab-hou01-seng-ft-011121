4688b907c4f93f7702eb6b291c37d24fd8b57007
Joshua Morales <joshuapmorales@gmail.com>
2020-12-28 19:12:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,17 @@
+
+
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  
+  new_hash = {:name => "Grace Hopper"}
+  
 end
 
+
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+
+  
 end
+
 
 def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
